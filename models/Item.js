82885e0a2277fbf1152ccb864d5a55b0369aba13
Prasoon Sinha <prasoonsinha@Prasoons-MBP.attlocal.net>
2021-02-21 12:45:30 -0600

    Design menu item model

---
+++ models/Item.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    // Please note this attribute is a link to a picture online
+    picture_link: {
+        type: String,
+        required: true,
+    },
+    item_name: {
+        type: String,
+        required: true,
+    },
+    item_cost: {
+        type: String,
+        required: true,
+    },
+    item_description: {
+        type: String,
+        required: true,
+    },
+    item_availability: {
+        type: String,
+        required: boolean,
+    },
+});
+
+module.exports = Item = mongoose.model('item', ItemSchema);
