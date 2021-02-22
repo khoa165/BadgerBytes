3a97d06cd2f64b7c279a5cb82defd2446f65a93b
wboettge <wboettge@wisc.edu>
2021-02-21 20:24:22 -0600

    hit API with auth request

--- client/src/Containers/Login/Login.js
+++ client/src/Containers/Login/Login.js
@@ -1,25 +1,75 @@
 import React,{Component} from 'react';
+import {Button, Form, FormGroup, Label, Input, Col} from 'reactstrap';
+import axios from 'axios';
 
 class Login extends Component{
+    state={
+        email:"",
+        password:""
+    }
+
+    submitForm(e) {
+        e.preventDefault()
+        console.log(`Email: ${ this.state.email }`)
+        console.log(`Password: ${ this.state.password }`)
+        axios.post('/api/v1/auth', {
+            credential: this.state.email,
+            password: this.state.password
+        }).then(res => {
+            console.log(res)
+            console.log(res.data)
+        })
+        axios.get('/api/v1/auth'
+        ).then(res => {
+            console.log(res)
+        })
+    }
+
+    handleChange = async (event) => {
+        const { target } = event;
+        const value = target.value;
+        const { name } = target;
+        this.setState({
+          [ name ]: value,
+        });
+    }
     
     render(){
         let login=(
-            <div className="login-wrapper">
-            <h1>Please Log In</h1>
-            <form>
-              <label>
-                <p>Username</p>
-                <input type="text" />
-              </label>
-              <label>
-                <p>Password</p>
-                <input type="password" />
-              </label>
-              <div>
-                <button type="submit">Submit</button>
-              </div>
-            </form>
-          </div>
+            <div className="loginForm">
+                <h2>Sign In</h2>
+                <Form className="form" onSubmit={ (e) => this.submitForm(e) }>
+                <Col>
+                    <FormGroup>
+                    <Label>Email</Label>
+                    <Input
+                        type="email"
+                        name="email"
+                        id="exampleEmail"
+                        placeholder="myemail@email.com"
+                        onChange={ (e) => {
+                            this.handleChange(e)
+                        } }
+                    />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input
+                        type="password"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="********"
+                        onChange={ (e) => {
+                            this.handleChange(e)
+                        } }
+                    />
+                    </FormGroup>
+                </Col>
+                <Button>Submit</Button>
+                </Form>
+            </div>
         );
 
         return(
