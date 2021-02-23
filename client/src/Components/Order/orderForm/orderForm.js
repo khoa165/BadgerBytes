e080f66e0c580b036d4066e78ec2e6cead59b919
Riza Hassan <ruhulruzbihan@gmail.com>
2021-02-23 15:59:55 -0600

    Changed to using Hooks

--- client/src/Components/Order/orderForm/orderForm.js
+++ client/src/Components/Order/orderForm/orderForm.js
@@ -1,52 +1,58 @@
-import React,{Component} from 'react';
+import React,{Component,useState} from 'react';
 import './orderForm.css';
 import {Container,Row,Col} from 'reactstrap';
 import Button from '../../buttons/grab-offer/grab-btn';
+import { Redirect } from "react-router-dom"; 
 
-class  orderForm extends Component{
+function orderForm2() {
 
-    constructor(props) {
-        super(props);
-        this.state = {name:"",
+    const [deliveryInfo,setDeliveryInfo] = useState([{
+        userName:"",
         phone:"",
         streetAddress:"",
         houseNumber:"",
         city:"",
-        zipcode:"",};
-      }
-    
-    nameHandler(e){
-        this.setState({name:e.target.value});
+        zipcode:"",
+    }])
+    const [isFilled,setIsFilled] = useState(false);
+
+    const nameHandler = (e) => {
+        setDeliveryInfo({userName:e.target.value})
     }
-    phoneHandler(e){
-        this.setState({phone:e.target.value});
+    const phoneHandler = (e) =>{
+        setDeliveryInfo({phone:e.target.value});
     }
-    streetHandler(e){
-        this.setState({streetAddress:e.target.value});
+    const streetHandler = (e) =>{
+        setDeliveryInfo({streetAddress:e.target.value});
     }
-    houseNumberHandler(e){
-        this.setState({houseNumber:e.target.value});
+    const houseNumberHandler = (e) =>{
+        setDeliveryInfo({houseNumber:e.target.value});
     }
-    cityHandler(e){
-        this.setState({city:e.target.value});
+    const cityHandler = (e) =>{
+        setDeliveryInfo({city:e.target.value});
     }
-    zipcodeHandler(e){
-        this.setState({zipcode:e.target.value});
+    const zipcodeHandler = (e) =>{
+        setDeliveryInfo({zipcode:e.target.value});
     }
 
-    render(){
-        var checkOut={
-            name:this.state.name,
-            phone:this.state.number,
-            streetAddress:this.state.streetAddress,
-            houseNumber:this.state.houseNumber,
-            city:this.state.city,
-            zipcode:this.state.zipcode
-        };
-        return(
-        <div className="OrderForm">
+    const onSubmit = (e) =>{
+        e.preventDefault()
+        console.log('HEREEE')
+        return <Redirect to="/payment" />
+        // if(deliveryInfo.name === "" || deliveryInfo.phone === "" || deliveryInfo.streetAddress === "" || deliveryInfo.houseNumber === "" ||deliveryInfo.city === "" || deliveryInfo.zipcode === ""){
+        //     setIsFilled(false)
+        //     alert("Please fill out all information");
+        // }else{
+        //     setIsFilled(true)
+        //     console.log("ehere");
+        //      return <Redirect to="/payment" />
+        // }
+    }
+    return (
+        <div>
+            <div className="OrderForm">
         <p className="OrderFormHead">Fill Delivery Details</p>
-            <form id="orderForm">
+            <form id="orderForm" onSubmit={onSubmit}>
                 <div className="form__container">
                 <Container>
                     <Row>
@@ -56,7 +62,7 @@ class  orderForm extends Component{
                             </legend>
                         </Col>
                         <Col xs="6" xl="8">
-                        <input type="text" value={this.state.name} onChange={this.nameHandler} id="CustomerName"/>
+                        <input type="text" value={deliveryInfo.userName} onChange={e=>nameHandler(e)} id="CustomerName"/>
                         </Col>
                     </Row>
                     <Row>
@@ -66,7 +72,7 @@ class  orderForm extends Component{
                             </legend>
                         </Col>
                         <Col xs="6" xl="8">
-                        <input type="text" value={this.state.phone} onChange={this.phoneHandler} id="Number"/>
+                        <input type="text" value={deliveryInfo.phone} onChange={e=>phoneHandler(e)} id="Number"/>
                         </Col>
                     </Row>
                     <Row>
@@ -76,7 +82,7 @@ class  orderForm extends Component{
                             </legend>
                         </Col>
                         <Col xs="6" xl="8">
-                        <input type="text" value={this.state.streetAddress} onChange={this.streetHandler} id="StreetAddress"/>
+                        <input type="text" value={deliveryInfo.streetAddress} onChange={e=>streetHandler(e)} id="StreetAddress"/>
                         </Col>
                     </Row>
 
@@ -87,7 +93,7 @@ class  orderForm extends Component{
                             </legend>
                         </Col>
                         <Col xs="6" xl="8">
-                        <input type="text" value={this.state.houseNumber} onChange={this.houseNumberHandler} id="HouseNumber"/>
+                        <input type="text" value={deliveryInfo.houseNumber} onChange={e=>houseNumberHandler(e)} id="HouseNumber"/>
                         </Col>
                     </Row>
                     <Row>
@@ -97,7 +103,7 @@ class  orderForm extends Component{
                             </legend>
                         </Col>
                         <Col xs="6" xl="8">
-                        <input type="text" value={this.state.city} onChange={this.cityHandler} id="City"/>
+                        <input type="text" value={deliveryInfo.city} onChange={e=>cityHandler(e)} id="City"/>
                         </Col>
                     </Row>
                     <Row>
@@ -107,20 +113,20 @@ class  orderForm extends Component{
                             </legend>
                         </Col>
                         <Col xs="6" xl="8">
-                        <input type="text" value={this.state.zipcode} onChange={this.zipcodeHandler} id="ZipCode"/>
+                        <input type="text" value={deliveryInfo.zipcode} onChange={e=>zipcodeHandler(e)} id="ZipCode"/>
                         </Col>
                     </Row>
 
  
                 </Container>
                 </div>
-                <Button type="button" url="/payment" content="Place Order"></Button>
+                <Button type="submit" content="PlaceOrder"></Button>
             </form>
             
             <p className="OrderFormNotice">*You will be able to select payment option.</p>
         </div>
-    );
-}
+        </div>
+    )
 }
 
-export default orderForm;
+export default orderForm2
